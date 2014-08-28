81ef256e0e966aef5f56471d5bfc53447d9f59bb
Alexander Markov <apsheronets@gmail.com>
2014-08-28 07:37:45 +0400

    fixes, total points, track segments

--- common.ml
+++ common.ml
@@ -6,7 +6,9 @@ let () =
   let x1 = ref 999.
   and x2 = ref (-999.)
   and y1 = ref 999.
-  and y2 = ref (-999.) in
+  and y2 = ref (-999.)
+  and points_count = ref 0
+  and track_segments = ref 0 in
   let rec skip i d =
     match Xmlm.input i with
     | `El_start ((_, s), _) -> skip i (d + 1)
@@ -19,14 +21,16 @@ let () =
           match Xmlm.input i with
           | `El_end -> ()
           | `El_start ((_, "trk"), _) ->
-              let rec pull i =
+              let rec pull_trkseg i =
                 match Xmlm.input i with
                 | `El_end -> ()
                 | `El_start ((_, "trkseg"), _) ->
+                    incr track_segments;
                     let rec pull i =
                       match Xmlm.input i with
                       | `El_end -> ()
                       | `El_start ((_, "trkpt"), attrs) ->
+                          incr points_count;
                           attrs >> List.iter (function
                             | ((_, "lon"), lon) ->
                                 let lon = float_of_string lon in
@@ -39,22 +43,24 @@ let () =
                             | _ -> ());
                           (match Xmlm.input i with
                           | `El_end -> pull i
-                          | `El_start _ ->  skip i 1; pull i
-                          | _ -> pull i)
+                          | `El_start _ -> skip i 2; pull i
+                          | _ -> skip i 1; pull i)
                       | `El_start _ -> skip i 1; pull i
                       | _ -> pull i in
-                    pull i
-                | `El_start _ -> skip i 1; pull i
-                | _ -> pull i in
-              pull i
+                    pull i; pull_trkseg i
+                | `El_start _ -> skip i 1; pull_trkseg i
+                | _ -> pull_trkseg i in
+              pull_trkseg i
           | `El_start _ -> skip i 1; pull i
           | _ -> pull i in
         pull i
     | `El_start _ -> skip i 1; pull i
     | _ -> pull i in
   pull i;
-  if !x1 = 999. || !y1 = 999. then exit 1;
+  if !points_count = 0 then (prerr_endline "no points"; exit 1);
   let lon = (!x1 +. !x2) /. 2.
   and lat = (!y1 +. !y2) /. 2. in
-  Printf.printf "Center point: %f %f\n" lon lat;
+  Printf.printf "center point: %f %f\n" lon lat;
+  Printf.printf "points count: %d\n" !points_count;
+  Printf.printf "track segments: %d\n" !track_segments;
   exit 0
